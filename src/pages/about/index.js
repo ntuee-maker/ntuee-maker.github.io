691f992af052e21978d42a4358ae2fd99a044c04
han0110 <qwerzxcv0110@gmail.com>
2018-04-12 10:57:31 +0800

    :lipstick: Add reset style

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,9 +2,11 @@
 
 import React from 'react';
 
+import styles from './about.module.scss';
+
 const Acativitys = () => (
-  <div>
-    About
+  <div className={styles.wrapper}>
+    <p>We are</p>
   </div>
 );
 
